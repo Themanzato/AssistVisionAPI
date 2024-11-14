a56685747024f51b0709887fb3953aca8140eae7
Manzato <mb13arroyo@gmail.com>
2024-11-13 22:44:41 -0600

    actualizo colores

--- pythonProject/app_local.py
+++ pythonProject/app_local.py
@@ -92,8 +92,6 @@ def determinar_nombre_color(avg_color):
         return "Dorado"
     elif r >= 140 and r <= 170 and g >= 110 and g <= 140 and b >= 60 and b <= 90:
         return "Café"
-    elif r > 100 and g > 80 and b > 40 and r < 120 and g < 100 and b < 60:
-        return "Café"
     elif r > 120 and g > 100 and b < 50 and r < 130 and g < 120 and b < 60:
         return "Café"
     elif r >= 150 and r <= 170 and g >= 150 and g <= 170 and b >= 140 and b <= 160:
@@ -114,8 +112,6 @@ def determinar_nombre_color(avg_color):
         return "Marrón"
     elif r >= 100 and r <= 120 and g >= 90 and g <= 110 and b >= 60 and b <= 80:
         return "Marrón"
-    elif r > 140 and g > 50 and b > 20 and r < 160 and g < 70 and b < 40:
-        return "Marrón"
     elif r >= 140 and r <= 150 and g >= 130 and g <= 140 and b >= 120 and b <= 130:
         return "Gris"
     elif r >= 150 and r <= 170 and g >= 180 and g <= 200 and b >= 100 and b <= 120:
@@ -134,16 +130,16 @@ def determinar_nombre_color(avg_color):
         return "Gris"
     elif r > 130 and g > 120 and b > 160 and r < 140 and g < 130 and b < 170:
         return "Gris"
-    elif r > 180 and g > 170 and b > 130 and r < 200 and g < 190 and b < 160:
-        return "Gris Claro"
-    elif r > 190 and g > 180 and b > 140 and r < 210 and g < 200 and b < 160:
-        return "Gris Claro"
     elif r >= 65 and r <= 80 and g >= 10 and g <= 25 and b >= 10 and b <= 25:
         return "Vino"
     elif r >= 90 and r <= 100 and g >= 15 and g <= 30 and b >= 10 and b <= 30:
         return "Vino"
     elif r >= 40 and r <= 60 and g >= 5 and g <= 20 and b >= 5 and b <= 20:
         return "Vino"
+    elif r > 10 and r < 40 and g > 70 and g < 90 and b > 160 and b < 190:
+        return "Azul"
+    elif r > 70 and r < 100 and g > 130 and g < 150 and b > 140 and b < 160:
+        return "Azul Marino"
     elif r > 30 and g > 30 and b > 50 and r < 70 and g < 50 and b < 100:
         return "Azul Marino"
     elif r > 60 and g > 60 and b > 120 and r < 100 and g < 100 and b < 150:
@@ -154,15 +150,13 @@ def determinar_nombre_color(avg_color):
         return "Azul Marino"
     elif r > 40 and g > 60 and b > 100 and r < 50 and g < 70 and b < 110:
         return "Azul Marino"
-    elif r > 10 and r < 40 and g > 70 and g < 90 and b > 160 and b < 190:
-        return "Azul"
     elif r > 50 and g < 30 and b > 20 and r < 70 and g < 30 and b < 50:
         return "Vino"
     elif r > 60 and g < 50 and b > 60 and r < 80 and g < 50 and b < 80:
         return "Morado"
+    elif r > 160 and g > 130 and b > 150 and r < 180 and g < 150 and b < 170:
+        return "Morado"
     elif r > 240 and g > 170 and b < 120 and r < 255 and g < 200 and b < 120:
-        return "Naranja"
-    elif r > 190 and g > 80 and b > 30 and r < 200 and g < 100 and b < 50:
         return "Naranja"
     elif r > 190 and g > 100 and b > 120 and r < 210 and g < 130 and b < 150:
         return "Rosa"
@@ -170,12 +164,13 @@ def determinar_nombre_color(avg_color):
         return "Rosa"
     elif r > 150 and g > 210 and b > 130 and r < 160 and g < 230 and b < 140:
         return "Amarillo Claro"
-
-
+    elif r > 60 and r < 80 and g > 130 and g < 150 and b > 85 and b < 100:
+        return "Verde Oscuro"
+    elif r > 45 and r < 60 and g > 65 and g < 80 and b > 100 and b < 115:
+        return "Azul Marino"
 
     else:
         return "indefinido"
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
